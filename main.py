8c8a8be98dd14b692250c62d247b70855f8d45e3
Minh Quang <96662798+Quang181@users.noreply.github.com>
2025-05-07 23:50:45 +0700

    Update main.py
    test

--- main.py
+++ main.py
@@ -2,12 +2,7 @@
 
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-a = 1
-b = a +1
-
-print("leu leu")
-def test():
-    print("test")
+print("sdasdsa
 
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
