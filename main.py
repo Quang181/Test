ebdca31722322cd8b972ee863eb4e609623bda49
Minh Quang <96662798+Quang181@users.noreply.github.com>
2025-05-07 23:38:00 +0700

    Update main.py

--- main.py
+++ main.py
@@ -2,6 +2,9 @@
 
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+a = 1
+b = a +1
+print("leu leu")
 def test():
     print("test")
 
